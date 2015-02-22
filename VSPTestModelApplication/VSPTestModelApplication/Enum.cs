898012b934e0965af6e4da74f54f4fd6a1587841
Uriah65 <uriah65@users.noreply.github.com>
2015-02-21 19:28:48 -0500

    .

--- VSPTestModelApplication/VSPTestModelApplication/Enum.cs
+++ VSPTestModelApplication/VSPTestModelApplication/Enum.cs
@@ -13,66 +13,18 @@ namespace VSPTestModelApplication
         [EnumSqlSelect("SELECT ContactTypeID, Name FROM Person.ContactType")]
         public enum ContactTypeEnum
         {
-            //AccountingManager = 1,
-            //AssistantSalesAgent = 2,
-            //AssistantSalesRepresentative = 3,
-            //CoordinatorForeignMarkets = 4,
-            //ExportAdministrator = 5,
-            //InternationalMarketingManager = 6,
-            //MarketingAssistant = 7,
-            //MarketingManager = 8,
-            //MarketingRepresentative = 9,
-            //OrderAdministrator = 10,
-            //Owner = 11,
-            //Owner_MarketingAssistant = 12,
-            //ProductManager = 13,
-            //PurchasingAgent = 14,
-            //PurchasingManager = 15,
-            //RegionalAccountRepresentative = 16,
-            //SalesAgent = 17,
-            //SalesAssociate = 18,
-            //SalesManager = 19,
-            //SalesRepresentative = 20,
         }
 
         [EnumSqlCnn("#CONFIG", @"Access")]
         [EnumSqlSelect("SELECT id, name, notes FROM T_Colors")]
         public enum AccessEnum
         {
-            //[Description("The color of tomato")]
-            //Red = 1,
-            //[Description("Sky color")]
-            //Blue = 2,
-            //[Description("Grass is green")]
-            //Green = 3,
         }
 
         [EnumSqlCnn("#CONFIG", @"Text")]
         [EnumSqlSelect(@"SELECT * FROM [data.csv]")]
         public enum OdbcEnum
         {
-            //[Description("#F0F8FF	 	Shades	Mix")]
-            //AliceBlue = 1,
-            //[Description("#FAEBD7	 	Shades	Mix")]
-            //AntiqueWhite = 2,
-            //[Description("#00FFFF	 	Shades	Mix")]
-            //Aqua = 3,
-            //[Description("#7FFFD4	 	Shades	Mix")]
-            //Aquamarine = 4,
-            //[Description("#F0FFFF	 	Shades	Mix")]
-            //Azure = 5,
-            //[Description("#F5F5DC	 	Shades	Mix")]
-            //Beige = 6,
-            //[Description("#FFE4C4	 	Shades	Mix")]
-            //Bisque = 7,
-            //[Description("#000000	 	Shades	Mix")]
-            //Black = 8,
-            //[Description("#FFEBCD	 	Shades	Mix")]
-            //BlanchedAlmond = 9,
-            //[Description("#0000FF	 	Shades	Mix")]
-            //Blue = 10,
-            //[Description("#8A2BE2")]
-            //BlueViolet = 11,
         }
 
     }
--- VSPTestModelApplication/VSPTestModelApplication/EnumExplicite.cs
+++ VSPTestModelApplication/VSPTestModelApplication/EnumExplicite.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,86 +15,18 @@ namespace VSPTestModelApplication
         [EnumSqlSelect("SELECT id, name, notes FROM T_Colors")]
         public enum AccessEnum
         {
-            //[Description("The color of tomato")]
-            //Red = 1,
-            //[Description("Sky color")]
-            //Blue = 2,
-            //[Description("Grass is green")]
-            //Green = 3,
         }
 
         [EnumSqlCnn("#ODBC", @"Driver={Microsoft Text Driver (*.txt; *.csv)};Dbq=c:\temp\;Extensions=asc,csv,tab,txt;Persist Security Info=False")]
         [EnumSqlSelect(@"SELECT * FROM [data.csv]")]
         public enum OdbcEnum
         {
-            //[Description("#F0F8FF	 	Shades	Mix")]
-            //AliceBlue = 1,
-            //[Description("#FAEBD7	 	Shades	Mix")]
-            //AntiqueWhite = 2,
-            //[Description("#00FFFF	 	Shades	Mix")]
-            //Aqua = 3,
-            //[Description("#7FFFD4	 	Shades	Mix")]
-            //Aquamarine = 4,
-            //[Description("#F0FFFF	 	Shades	Mix")]
-            //Azure = 5,
-            //[Description("#F5F5DC	 	Shades	Mix")]
-            //Beige = 6,
-            //[Description("#FFE4C4	 	Shades	Mix")]
-            //Bisque = 7,
-            //[Description("#000000	 	Shades	Mix")]
-            //Black = 8,
-            //[Description("#FFEBCD	 	Shades	Mix")]
-            //BlanchedAlmond = 9,
-            //[Description("#0000FF	 	Shades	Mix")]
-            //Blue = 10,
-            //[Description("#8A2BE2")]
-            //BlueViolet = 11,
         }
 
         [EnumSqlCnn("(local)", "AdventureWorks2014")]
         [EnumSqlSelect("SELECT ContactTypeID, Name, Name FROM Person.ContactType")]
         public enum ContactType2Enum
         {
-            //[Description("Accounting Manager")]
-            //AccountingManager = 1,
-            //[Description("Assistant Sales Agent")]
-            //AssistantSalesAgent = 2,
-            //[Description("Assistant Sales Representative")]
-            //AssistantSalesRepresentative = 3,
-            //[Description("Coordinator Foreign Markets")]
-            //CoordinatorForeignMarkets = 4,
-            //[Description("Export Administrator")]
-            //ExportAdministrator = 5,
-            //[Description("International Marketing Manager")]
-            //InternationalMarketingManager = 6,
-            //[Description("Marketing Assistant")]
-            //MarketingAssistant = 7,
-            //[Description("Marketing Manager")]
-            //MarketingManager = 8,
-            //[Description("Marketing Representative")]
-            //MarketingRepresentative = 9,
-            //[Description("Order Administrator")]
-            //OrderAdministrator = 10,
-            //[Description("Owner")]
-            //Owner = 11,
-            //[Description("Owner/Marketing Assistant")]
-            //Owner_MarketingAssistant = 12,
-            //[Description("Product Manager")]
-            //ProductManager = 13,
-            //[Description("Purchasing Agent")]
-            //PurchasingAgent = 14,
-            //[Description("Purchasing Manager")]
-            //PurchasingManager = 15,
-            //[Description("Regional Account Representative")]
-            //RegionalAccountRepresentative = 16,
-            //[Description("Sales Agent")]
-            //SalesAgent = 17,
-            //[Description("Sales Associate")]
-            //SalesAssociate = 18,
-            //[Description("Sales Manager")]
-            //SalesManager = 19,
-            //[Description("Sales Representative")]
-            //SalesRepresentative = 20,
         }
     }
 }
