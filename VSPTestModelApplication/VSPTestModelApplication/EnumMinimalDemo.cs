d5a0227ba032cf91146c3e3c6870efdc021a7a7d
Uriah65 <Uriah65@hotmail.com>
2015-03-03 00:21:08 +0000

    .

---
+++ VSPTestModelApplication/VSPTestModelApplication/EnumMinimalDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VSPTestModelAppLib;
+
+namespace VSPTestModelApplication
+{
+    class EnumMinimalDemo
+    {
+        [EnumSqlCnn("FakeSqlServer", "FakeDb")]
+        [EnumSqlSelect("SELECT id, name, description FROM T_Weekdays")]
+        public enum Weekdays
+        {
+        }
+
+        [EnumSqlCnn("FakeSqlServer", "FakeDb")]
+        [EnumSqlSelect("SELECT id, name FROM T_Weekdays")]
+        public enum Weekdays2
+        {
+        }
+    }
+}
